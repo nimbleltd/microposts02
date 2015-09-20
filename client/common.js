6bb63cb0d5942d56d26f03564449b1da2e2dab48
Paul Campbell <nimbleltd@gmail.com>
2015-09-20 09:21:32 -0500

    Added collection2 and autoform packages

--- client/common.js
+++ client/common.js
@@ -1,6 +1,11 @@
 Meteor.startup(function () {
     AccountsEntry.config({
       homeRoute: '/',                    // mandatory - path to redirect to after sign-out
-      dashboardRoute: '/'      // mandatory - path to redirect to after successful sign-in
+      dashboardRoute: '/',      // mandatory - path to redirect to after successful sign-in
+      passwordSignupFields: 'USERNAME_AND_EMAIL'
+    });
+
+    Accounts.ui.config({
+    	passwordSignupFields: 'USERNAME_AND_EMAIL'
     });
   });
