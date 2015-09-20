9df1117489c29d1cb3b8ce8e77be7f3f8adf8516
Paul Campbell <nimbleltd@gmail.com>
2015-09-20 17:18:36 -0500

    added ability to post text via the modal

--- client/common.js
+++ client/common.js
@@ -9,3 +9,9 @@ Meteor.startup(function () {
     	passwordSignupFields: 'USERNAME_AND_EMAIL'
     });
   });
+
+
+Template.registerHelper('getProfileImg', function(userId){
+	var imgUrl = UserImages.findOne({userId: userId}).image;
+	return imgUrl;
+});
